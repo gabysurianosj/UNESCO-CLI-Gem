5d417c0c6b18da99dd37186639e5969138221432
Gabriela Suriano <surianogabriela@gmail.com>
2018-11-08 22:42:38 +0000

    styling

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,41 +1,43 @@
 class Controller
 
   def initialize
-    puts "Welcome to UNESCO World Heritage Sites!"
+    puts "\nWelcome to UNESCO World Heritage Sites!".light_blue
+    puts "-------------------------------------------"
     Scraper.make_sites
   end
 
   def list_options(start_number)
-    puts "---------- UNESCO Heritage Sites #{start_number} - #{start_number+99} ----------"
+    puts "\n---------- UNESCO Heritage Sites #{start_number} - #{start_number+99} ----------".light_blue
     Site.all[start_number-1, 100].each.with_index(start_number) do |site, i|
       puts "#{i}. #{site.title}"
     end
   end
 
   def call
-    puts "What sites would you like to see? 1-100, 101-200, 201-300, ... 1001-1100"
+    puts "\nWhat sites would you like to see?".cyan
+    puts "\n1-100, 101-200, 201-300,..., 1001-1100 - choose a category:".cyan
     input = gets.strip.to_i
     list_options(input)
-    puts "Type the site number you would like more info on or exit:"
+    puts "\nType the 'site number' you would like more information on, or 'exit':".cyan
     input = nil
     while input != "exit"
       input = gets.strip.downcase
       if input.to_i > 0 && input.to_i <= Site.all.count
         site = Site.all[input.to_i - 1]
         puts ""
-        puts "#{site.title}"
+        puts "\n#{site.title}".light_blue
         puts ""
         puts "#{site.description}"
         puts ""
-        puts "Type list to see the options again or exit."
+        puts "\nType 'list' to see the options again or 'exit'.".cyan
       elsif input == "list"
         call
       else
         break if input == "exit"
-        puts "Type a valid integer or exit."
+        puts "\nType a valid integer or exit.".red
       end
     end
-    puts "Goodbye and happy learning!"
+    puts "\nI hope we were helpful. Safe travels!".cyan
   end
 
 end
